1909330231363670ebfc135972a48603c51b1add
SeokHo-Ham <kalejuice02@gmail.com>
2023-01-22 16:28:59 +0900

    @SpringBootApplication 어노테이션을 사용하지 않고 톰캣(서블릿 컨테이너) 실행하기

--- src/main/java/com/tobyspringboot/TobySpringBootApplication.java
+++ src/main/java/com/tobyspringboot/TobySpringBootApplication.java
@@ -1,15 +1,37 @@
 package com.tobyspringboot;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.catalina.LifecycleException;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 public class TobySpringBootApplication {
 
     public static void main(String[] args) throws LifecycleException {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+
+                    PrintWriter writer = resp.getWriter();
+                    writer.println("Hello " + name);
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 
